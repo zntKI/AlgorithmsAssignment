a119002391401c38e858b9e2c3039208ee76d979
zntKI <kaloyanivanov@hotmail.com>
2024-05-23 13:23:15 +0200

    Created SufficientDungeon class

---
+++ assignment/sources/Solution/Dungeon/SufficientDungeon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SufficientDungeon : Dungeon
+{
+    public SufficientDungeon(Size pSize) : base(pSize)
+    {
+    }
+
+    protected override void generate(int pMinimumRoomSize)
+    {
+        
+    }
+}
